d0e65b4fddfa42b92d3ae7132025aff04304f5a8
Wang <wan333@csiro.au>
2019-09-25 16:42:44 +1000

    update 25/09/2019

--- script/Climate.R
+++ script/Climate.R
@@ -3,3 +3,11 @@ library(tidyverse)
 BOM_data <- read_csv("data/BOM_data.csv")
 BOM_stations <- read_csv("data/BOM_stations.csv")
 
+BOM_data_separate <- 
+  separate(BOM_data, col = Temp_min_max, into = c("Temp_min", "Temp_max"), sep = "/")
+BOM_data_separate
+
+BOM_data_separate %>% 
+  filter(Temp_min == min(Temp_min), Temp_max == max(Temp_max)) %>%
+  group_by(Station_number) %>% 
+  summarise(num_rows = n())
