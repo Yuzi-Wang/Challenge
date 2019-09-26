6d7d0963006ef8b2d8b4918c35105c4389ce3827
Wang <wan333@csiro.au>
2019-09-26 15:34:56 +1000

    Update 26/09/22019

--- script/Climate.R
+++ script/Climate.R
@@ -1,24 +1,32 @@
 library(tidyverse)
 
 #Q1
+
 BOM_data <- read_csv("data/BOM_data.csv")
 BOM_stations <- read_csv("data/BOM_stations.csv")
 
 BOM_data_separate <- 
   separate(BOM_data, col = Temp_min_max, into = c("Temp_min", "Temp_max"), sep = "/")
+# split the col into two diff one
+
 BOM_data_separate
 
 View(BOM_data_separate)
+
 BOM_data_separate %>% 
   filter(Temp_min >= 0, Temp_max >= 0, Rainfall >= 0) %>% 
+  # retain the raws that have values
   group_by(Station_number) %>% 
-  summarise(num_rows = n())
+  # group by the satation
+  summarise(num_rows = n()) # count the nbr of days
 
 #Q2
+
 BOM_data_tidy <- BOM_data_separate %>% 
   filter(Temp_min >= 0, Temp_max >= 0, Rainfall >= 0)
+# Save this filtered data frame
+
 BOM_data_tidy
-# retain the raws which havr values
 
 BOM_data_num <- BOM_data_tidy %>% 
   mutate(
@@ -33,13 +41,48 @@ Data_with_Temp_diff <- mutate(BOM_data_num, Temp_diff = Temp_max - Temp_min)
 # add a new col and calculate the difference 
 
 Data_with_Temp_diff %>% 
-  group_by(Month) %>%
-  summarise(Mean_temp = mean(Temp_diff)) %>% 
-  arrange(Mean_temp)
+  group_by(Month) %>% # group by monthe
+  summarise(Mean_temp = mean(Temp_diff)) %>% # caculate the average for each month
+  arrange(Mean_temp) # order the average
+
 
 #Q3:
 
-BOM_stations
-view(BOM_stations)
+BOM_station_tidy <- BOM_stations %>% 
+  gather(Station_number, number, -info) %>% 
+  spread(info, number)
+#tidy the data
 
-gather(-info)
+BOM_station_tidy
+
+BOM_station_num <- BOM_station_tidy %>% 
+  mutate(Station_number = as.numeric(Station_number))
+#change the chr into num for the next join step
+
+BOM_station_num
+
+joined_BOM <- full_join(BOM_station_num, Data_with_Temp_diff)
+#join the two dataset
+
+view(joined_BOM)
+
+joined_BOM %>% 
+  group_by(state) %>% # group by state
+  summarise(mean_Temp_diff = mean(Temp_diff)) %>% # caculate the average for each state
+  arrange(mean_Temp_diff)# order the average
+
+# Q4:
+
+joined_BOM %>% 
+  filter(Solar_exposure != "NA") %>% 
+  mutate(lon = as.numeric(lon)) %>% 
+  filter(lon == max(lon)) %>% 
+  summarise(mean_solar = mean(Solar_exposure))
+
+joined_BOM %>% 
+  filter(Solar_exposure != "NA") %>% 
+  mutate(lon = as.numeric(lon)) %>% 
+  filter(lon == min(lon)) %>% 
+  summarise(mean_solar = mean(Solar_exposure))
+
+  
